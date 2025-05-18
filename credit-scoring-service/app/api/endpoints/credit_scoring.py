ace995d3990078c2eb16e4ad880d4374f4479d2e
Danis Sharafiev <harnemer@gmail.com>
2025-05-18 21:23:06 +0300

    fix(project): solve issues with models path, json format

--- credit-scoring-service/app/api/endpoints/credit_scoring.py
+++ credit-scoring-service/app/api/endpoints/credit_scoring.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, Request, HTTPException
 from app.schema.prediction import PredictionRequest, PredictionResponse
 from models.inference import model
+import json
+import pandas as pd
 
-router = APIRouter(prefix="/api/", tags=["credit-scoring"])
+router = APIRouter(prefix="/api", tags=["credit-scoring"])
 
 @router.get("/health")
 async def health_check():
@@ -14,10 +16,12 @@ async def predict_credit_score(
     req: Request):
     try:
         req.app.state.logger.info(f"Received request: {request.model_dump_json()}")
-        json_data = request.model_dump_json()
-        X = [json_data["interest_rate"], json_data["outstanding_debt"]]
+        json_data = json.loads(request.model_dump_json())
+        X = pd.DataFrame({"Interest_Rate": [json_data["interest_rate"]], "Outstanding_Debt": [json_data["outstanding_debt"]]})
         req.app.state.logger.info(f"Data for prediction: {X}")
-        response = PredictionResponse("Good" if int(model.predict(X)[0]) else "Bad")
+        prediction = "Good" if int(model.predict(X)[0]) else "Bad"
+        req.app.state.logger.info(f"Prediction result: {prediction}")
+        response = PredictionResponse(credit_score=prediction)
         req.app.state.logger.info(f"Prediction response: {response.model_dump_json()}")
     except Exception as e:
         req.app.state.logger.error(f"Error during prediction: {str(e)}")
--- credit-scoring-service/app/core/config.py
+++ credit-scoring-service/app/core/config.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
 class Config:
-    MODEL_PATH = Path(__file__).parent / "model.pkl"
+    MODEL_PATH = Path(__file__).parents[2] / "models" / "model.pkl"
 
-    
